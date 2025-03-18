06178c5e2b4db37fc9387923c532404e3b7dd99c
firas <firashedfi4@gmail.com>
2025-03-19 00:27:32 +0100

    Added the appbar for the client_home_screen.dart

--- lib/screens/client/client_home_screen.dart
+++ lib/screens/client/client_home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _auth = FirebaseAuth.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,9 +13,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Stream<DocumentSnapshot> userStream;
+  final user = _auth.currentUser!;
+
+  @override
+  void initState() {
+    super.initState();
+    userStream = _firestore.collection("clients").doc(user.uid).snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications_rounded)),
+        ],
+        title: StreamBuilder<DocumentSnapshot>(
+          stream: userStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            }
+
+            if (!snapshot.hasData || !snapshot.data!.exists) {
+              return const Center(child: Text("Aucune donnée trouvée"));
+            }
+
+            // Extract user data from DocumentSnapshot
+            final userData = snapshot.data!.data() as Map<String, dynamic>;
+
+            return Row(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  foregroundImage:
+                      userData['Photo de profile'] != null
+                          ? NetworkImage(userData['Photo de profile'])
+                          : null,
+                  child:
+                      userData['Photo de profile'] == null
+                          ? const Icon(
+                            Icons.person,
+                            size: 70,
+                            color: Colors.white,
+                          )
+                          : null,
+                ),
+                SizedBox(width: 10),
+                Text.rich(
+                  TextSpan(
+                    text: 'Salut, ', // Normal text
+                    style:
+                        Theme.of(
+                          context,
+                        ).textTheme.titleMedium, // Default style
+                    children: [
+                      TextSpan(
+                        text: userData['Nom d\'utilisateur'], // Bold text
+                        style: Theme.of(context).textTheme.titleMedium
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
