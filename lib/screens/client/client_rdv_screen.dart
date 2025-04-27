244297c6e28fecb8f20da545c43d22be18c6a89f
firas <firashedfi4@gmail.com>
2025-04-27 20:18:01 +0100

    Refactor ClientRdvScreen; update layout to use NestedScrollView and TabBar for improved navigation and user experience

--- lib/screens/client/client_rdv_screen.dart
+++ lib/screens/client/client_rdv_screen.dart
@@ -7,15 +7,76 @@ class ClientRdvScreen extends StatefulWidget {
   State<ClientRdvScreen> createState() => _ClientRdvScreenState();
 }
 
-class _ClientRdvScreenState extends State<ClientRdvScreen> {
+class _ClientRdvScreenState extends State<ClientRdvScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+    ); // 👈 Adjust length based on number of tabs
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Mes rendez-vous')),
-      body: Center(
-        child: Text(
-          'Pas encore de rendez-vous',
-          style: Theme.of(context).textTheme.titleMedium,
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder:
+            (context, innerBoxIsScrolled) => [
+              SliverAppBar(
+                title: Text('Mes réservations'),
+                pinned: true,
+                floating: true,
+                bottom: TabBar(
+                  labelStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  // indicatorColor: Colors.transparent,
+                  dividerColor: Colors.transparent,
+                  controller: _tabController,
+                  tabs: [
+                    Tab(text: 'À venir'),
+                    Tab(text: 'Terminé'),
+                    Tab(text: 'Annulé'),
+                  ],
+                ),
+              ),
+            ],
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            Center(
+              child: Text(
+                'Aucun réservation à venir',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+
+            Center(
+              child: Text(
+                'Aucun réservation terminé',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+
+            Center(
+              child: Text(
+                'Aucun réservation annulé',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+          ],
         ),
       ),
     );
