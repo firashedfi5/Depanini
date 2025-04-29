36cfb70b18beb84efd462945997b93b23e7dc1f9
firas <firashedfi4@gmail.com>
2025-04-29 11:04:23 +0100

    Refactor AdminHomeScreen; replace Container with Scaffold for improved layout structure and enhance sign-out functionality

--- lib/screens/admin/admin_home_screen.dart
+++ lib/screens/admin/admin_home_screen.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+final _auth = FirebaseAuth.instance;
+
 class AdminHomeScreen extends StatefulWidget {
-  const AdminHomeScreen({ super.key });
+  const AdminHomeScreen({super.key});
 
   @override
   State<AdminHomeScreen> createState() => _AdminHomeScreenState();
@@ -10,8 +13,34 @@ class AdminHomeScreen extends StatefulWidget {
 class _AdminHomeScreenState extends State<AdminHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Admin Home Screen',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            SizedBox(height: 20),
+            TextButton.icon(
+              onPressed: () {
+                _auth.signOut();
+              },
+              icon: Icon(Icons.logout, color: Colors.red, size: 25),
+              style: TextButton.styleFrom(),
+              label: Text(
+                "Se déconnecter",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
